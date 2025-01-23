b6c3ca76c6281b72b8c1f29f0a5e03a1d7b884fe
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2025-01-23 14:39:25 +0100

    create and complete general settings component for settings component

--- src/components/popups/Networks.tsx
+++ src/components/popups/Networks.tsx
@@ -4,6 +4,7 @@ import { networks } from "../../data";
 import NetworkItem from "./NetworkItem";
 import useDelayedHover from "../../hooks/useDelayedHover";
 import NetworkItemAdd from "./NetworkItemAdd";
+import ToggleButton from "../../ui/ToggleButton";
 
 function Networks() {
   const [infoCordTop, setInfoCordTop] = useState(0);
@@ -140,40 +141,7 @@ function Networks() {
         <p className="sm:text-sm md:text-[1rem] md:leading-6 font-medium text-secondary-500">
           Show test networks
         </p>
-        <label className="flex cursor-pointer">
-          <div className="flex justify-start items-center w-[52px] bg-transparent p-0 border-0 relative cursor-pointer select-none">
-            <div
-              className={`w-[40px] h-[24px] p-0 flex justify-center items-center border-none rounded-[26px] ${
-                isShowTestNetwork ? "bg-brand-500" : "bg-secondary-500"
-              } `}
-            >
-              <div
-                className={`flex justify-center items-center relative font-[Helvetica Neue,_Helvetica,_sans-serif] mt-auto mb-auto leading-[0] w-[26px] h-[20px] left-1 ${
-                  isShowTestNetwork ? "opacity-100" : "opacity-0"
-                }`}
-              ></div>
-              <div
-                className={`flex justify-center items-center relative font-[Helvetica Neue,_Helvetica,_sans-serif] mt-auto mb-auto pr-[5px] leading-[0] w-[26px] h-[20px] bottom-0 text-[rgba(255,_255,_255,_0.6)] ${
-                  isShowTestNetwork ? "opacity-0" : "opacity-100"
-                }`}
-              ></div>
-            </div>
-            <div className="absolute top-0 left-0 h-full flex justify-start items-center self-stretch flex-1">
-              <div
-                className={`w-[18px] h-[18px] flex self-center shadow-[0px_2px_4px_0px_rgba(0,0,0,0.1)] rounded-[50%] box-border relative bg-white transition-[left_250ms_linear] ${
-                  isShowTestNetwork ? "left-[18px]" : "left-[3px]"
-                }`}
-              ></div>
-            </div>
-            <input
-              type="checkbox"
-              value={isShowTestNetwork ? "true" : "false"}
-              className="w-px h-px absolute p-0 -m-[1px] overflow-hidden"
-              style={{ clip: "rect(0px, 0px, 0px, 0px)" }}
-              onClick={handleToggle}
-            />
-          </div>
-        </label>
+        <ToggleButton isToggled={isShowTestNetwork} onToggle={handleToggle} />
       </div>
       {isShowTestNetwork && (
         <div className="h-full overflow-auto">
--- src/components/settings/General.tsx
+++ src/components/settings/General.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import Icon from "../../ui/Icon";
+import ToggleButton from "../../ui/ToggleButton";
 
 function General() {
+  const [isShowNativeToken, setIsShowNativeToken] = useState(false);
+  const [isZeroBalance, setIsZeroBalance] = useState(false);
+  const [isActiveIdenticon, setIsActiveIdenticon] = useState(0);
+
+  function handleTokenToggle() {
+    setIsShowNativeToken((prevState) => !prevState);
+  }
+  function toggleZeroBalance() {
+    setIsZeroBalance((prevState) => !prevState);
+  }
+
+  function handleIdenticonSelection(index: number) {
+    setIsActiveIdenticon(index);
+  }
+
   return (
     <div className="settings-body p-4 pt-0">
       <div className="flex flex-col p-0 pt-4">
@@ -18,20 +35,178 @@ function General() {
                 className="w-full py-3 pr-[40px] pl-4 bg-white rounded-lg text-secondary-900 border border-solid appearance-none text-sm leading-[140%] border-[rgba(187,192,197,0.9)]"
               >
                 <option value="">USD - United States Dollar</option>
+                <option value="">EUR - Euro</option>
               </select>
-              <Icon imgUrl="src/assets/images/arrow-down.svg" />
+              <div className="absolute top-[50%] right-4 translate-y-[-50%] pointer-events-none">
+                <Icon imgUrl="src/assets/images/arrow-down.svg" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-row p-0 pt-4">
-        <div className="flex flex-col min-w-0"></div>
-        <div className="flex flex-col min-w-0"></div>
+      <div className="flex flex-row justify-between items-center p-0 pt-4">
+        <div className="flex flex-col min-w-0">
+          <p className="text-secondary-900 text-sm leading-[22px] font-medium">
+            Show native token as main balance
+          </p>
+        </div>
+        <div className="flex flex-col max-w-[300px] mt-2">
+          <ToggleButton
+            isToggled={isShowNativeToken}
+            onToggle={handleTokenToggle}
+          />
+        </div>
       </div>
-      <div className="flex flex-col p-0 pt-4"></div>
-      <div className="flex flex-col p-0 pt-4"></div>
-      <div className="flex flex-col p-0 pt-4"></div>
-      <div className="flex flex-row p-0 pt-4"></div>
+      <div className="flex flex-col p-0 pt-4">
+        <div className="flex flex-col min-w-0">
+          <p className="text-secondary-900 text-sm leading-[22px] font-medium">
+            Current language
+          </p>
+        </div>
+        <div className="flex flex-col min-w-0">
+          <div className="flex flex-col mt-2 max-w-[300px]">
+            <div className="inline-block h-[48px] relative">
+              <select
+                name=""
+                id=""
+                className="w-full py-3 pr-[40px] pl-4 bg-white rounded-lg text-secondary-900 border border-solid appearance-none text-sm leading-[140%] border-[rgba(187,192,197,0.9)]"
+              >
+                <option value="">English</option>
+                <option value="">German</option>
+                <option value="">French</option>
+                <option value="">Spanish</option>
+                <option value="">Italian</option>
+              </select>
+              <div className="absolute top-[50%] right-4 translate-y-[-50%] pointer-events-none">
+                <Icon imgUrl="src/assets/images/arrow-down.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-0 pt-4">
+        <div className="flex flex-col min-w-0">
+          <p className="text-secondary-900 text-sm leading-[22px] font-medium">
+            Theme
+          </p>
+          <div className="text-secondary-600 text-sm leading-[22px]">
+            Choose your preferred Walleth theme.
+          </div>
+        </div>
+        <div className="flex flex-col min-w-0">
+          <div className="flex flex-col mt-2 max-w-[300px]">
+            <div className="inline-block h-[48px] relative">
+              <select
+                name=""
+                id=""
+                className="w-full py-3 pr-[40px] pl-4 bg-white rounded-lg text-secondary-900 border border-solid appearance-none text-sm leading-[140%] border-[rgba(187,192,197,0.9)]"
+              >
+                <option value="">Light</option>
+                <option value="">Dark</option>
+                <option value="">System</option>
+              </select>
+              <div className="absolute top-[50%] right-4 translate-y-[-50%] pointer-events-none">
+                <Icon imgUrl="src/assets/images/arrow-down.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-0 pt-4">
+        <div className="flex flex-col min-w-0">
+          <p className="text-secondary-900 text-sm leading-[22px] font-medium">
+            Account identicon
+          </p>
+          <p className="text-secondary-600 text-sm leading-[22px] mb-3">
+            Jazzicons and Blockies are two different styles of unique icons that
+            help you identify an account at a glance.
+          </p>
+          <div className="flex flex-row items-baseline">
+            <button
+              className="flex flex-row justify-between items-center bg-[unset] border border-solid border-transparent cursor-pointer"
+              onClick={() => handleIdenticonSelection(0)}
+            >
+              <div
+                className={`flex justify-center items-center w-[40px] h-[40px] rounded-[40px] ${
+                  isActiveIdenticon === 0
+                    ? "border-[2px] border-solid border-brand-500"
+                    : "border-none"
+                }`}
+              >
+                <div className="block w-[32px] h-[32px] rounded-2xl">
+                  <div className="inline-block w-[32px] h-[32px] m-0 p-0 overflow-hidden rounded-[50px] bg-[rgb(245,204,0)]">
+                    <svg x="0" y="0" width="32" height="32">
+                      <rect
+                        x="0"
+                        y="0"
+                        width="32"
+                        height="32"
+                        transform="translate(-0.035184136459420805 2.3797891171110326) rotate(128.5 16 16)"
+                        fill="#1893F2"
+                      ></rect>
+                      <rect
+                        x="0"
+                        y="0"
+                        width="32"
+                        height="32"
+                        transform="translate(-12.423438862341186 1.2328918067628662) rotate(244.6 16 16)"
+                        fill="#FA7D00"
+                      ></rect>
+                      <rect
+                        x="0"
+                        y="0"
+                        width="32"
+                        height="32"
+                        transform="translate(7.374821524232092 24.9931280713878) rotate(172.8 16 16)"
+                        fill="#C81447"
+                      ></rect>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <h6 className="mr-12 ml-3 my-0 text-secondary-900 text-xs leading-5 md:text-sm md:leading-snug">
+                Jazzicons
+              </h6>
+            </button>
+            <button
+              className="flex flex-row justify-between items-center bg-[unset] border border-solid border-transparent cursor-pointer"
+              onClick={() => handleIdenticonSelection(1)}
+            >
+              <div
+                className={
+                  isActiveIdenticon
+                    ? "flex justify-center items-center w-[40px] h-[40px] rounded-[40px] border-[2px] border-solid border-brand-500"
+                    : ""
+                }
+              >
+                <img
+                  src="src/assets/images/blockie.svg"
+                  alt="Blockies identicon"
+                  width="32px"
+                  height="32px"
+                  style={{ borderRadius: "50%" }}
+                />
+              </div>
+              <h6 className="mr-0 ml-3 my-3 text-secondary-900 text-xs leading-5 md:text-sm md:leading-snug">
+                Blockies
+              </h6>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between items-center p-0 pt-4">
+        <div className="flex flex-col min-w-0">
+          <p className="text-secondary-900 text-sm leading-[22px] font-medium">
+            Hide tokens without balance
+          </p>
+        </div>
+        <div className="flex flex-col max-w-[300px] mt-2">
+          <ToggleButton
+            isToggled={isZeroBalance}
+            onToggle={toggleZeroBalance}
+          />
+        </div>
+      </div>
     </div>
   );
 }
